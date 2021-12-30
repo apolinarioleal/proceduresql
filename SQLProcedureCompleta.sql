76a3f7498717ba8314f5d175969109c77f7327d5
Apolinário Leal <apolinarioleal@gmail.com>
2021-12-30 11:03:59 -0300

    Add files via upload

--- SQLProcedureCompleta.sql
+++ SQLProcedureCompleta.sql
@@ -1,21 +1,43 @@
+USE [controleponto]
+GO
+
+/****** Object:  StoredProcedure [dbo].[Proc_departamento]    Script Date: 30/12/2021 11:01:53 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[Proc_departamento]( @vDepartamentoID INT, @VResponsavel varchar(20), @VLogin varchar (10), @VEmail varchar (20))
+
+AS
+BEGIN
+
 declare @Existe int  =0 
 declare @Retorno varchar(50)=''
-/* Teste se o codigo 0001 existe*/
-set @Existe= (select count(1) from [controleponto].[dbo].[TapDepartamento] where DepartamentoID  = '2')
+
+
+set @Existe= (select count(1) from [controleponto].[dbo].[TapDepartamento] where DepartamentoID  = @vDepartamentoID)
 
 
 if ( @Existe = 0) /* se não existe Insere um novo*/
   begin
-     Insert into [controleponto].[dbo].[TapDepartamento] (DepartamentoID, Responsavel,Login, Email) values ('2', 'apolinario', 'sistema','leal@gmail.com')
+     Insert into [controleponto].[dbo].[TapDepartamento] (DepartamentoID, Responsavel,Login, Email) values (@vDepartamentoID, @VResponsavel, @VLogin, @VEmail)
 	 set @Retorno='Registro Inserido' 
   end
   else begin /* se  existe Altera*/
-			Update [controleponto].[dbo].[TapDepartamento] 
-			set DepartamentoID = '1', 
-				Responsavel = 'Leal',
-				Login = 'teste',
-				Email = 'teste@gmail.com'
+			Update [controleponto].[dbo].[TabDepartamento] 
+			set 
+				Responsavel = @VResponsavel,
+				Login = @VLogin,
+				Email = @VEmail
 								
-				where DepartamentoID = '1'  	 
+				where DepartamentoID = @vDepartamentoID 	 
 			set @Retorno='Registro Alterado'
        end
+
+	select @Retorno RETORNO
+END
+GO
+
+
